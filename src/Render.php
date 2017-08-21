a68a61d633aa5b0ced3481ed2a455335ffdc0e98
Erayd <steve@erayd.net>
2017-08-21 23:50:54 +1200

    Add fancy emdash

--- src/Render.php
+++ src/Render.php
@@ -37,21 +37,23 @@ abstract class Render
     protected function smartPunctuation(string $paragraph) : string
     {
         $map = [
-            '/\s+/u' => ' ',                                       // normalise whitespace
-            '/["\x{201D}\x{201C}]+/u' => '"',                      // normalise double-quotes
-            '/"(.+?)"/u' => '“$1”',                                // fancy double-quote pairs
-            '/^"/u' => '“',                                        // fancy double-quote paragraph start
-            "/['\x{2018}\x{2019}]+/u" => "'",                      // normalise single-quotes
-            "/(?<=[\w])'(?=[\w])/u" => "’",                        // fancy apostrophes
-            "/(?<=s)'(?=[\s,.;:-\x{2014}?\x{201D}]|$)/u" => "’",   // fancy possessive apostrophes
-            "/'(.+?)'/u" => '‘$1’',                                // fancy single-quote pairs
-            '/(?<=[^.]|^)\s*(?:\.(\s*)){3,5}(?=[^.]|$)/u' => '… ', // fancy ellipses
-            '/\s+([.,;:!?\x{2026}])/u' => '$1',                    // correct preceeding whitespace
-            '/(?<=[\w])\(\s+/u' => ' (',                           // correct preceding whitespace
-            '/([,.;:!?)\x{2026}])\s*(?=[\w])/u' => '$1 ',          // correct trailing whitespace
-            '/(\w\.)\s*(?=\w\.)/u' => '$1',                        // correct acronym period spacing
-            '|(?<=[\w])\s*/\s*(?=[\w])|u' => ' / ',                // correct surrounding whitespace
-            '/^\s*(.+?)\s*$/u' => "$1",                            // remove wrapping whitespace
+            '/\s+/u' => ' ',                                        // normalise whitespace
+            '/["\x{201D}\x{201C}]+/u' => '"',                       // normalise double-quotes
+            '/"(.+?)"/u' => '“$1”',                                 // fancy double-quote pairs
+            '/^"/u' => '“',                                         // fancy double-quote paragraph start
+            "/['\x{2018}\x{2019}]+/u" => "'",                       // normalise single-quotes
+            "/(?<=[\w])'(?=[\w])/u" => "’",                         // fancy apostrophes
+            "/(?<=s)'(?=[\s,.;:-\x{2014}?\x{201D}]|$)/u" => "’",    // fancy possessive apostrophes
+            "/'(.+?)'/u" => '‘$1’',                                 // fancy single-quote pairs
+            '/(?<=[^.]|^)\s*(?:\.(\s*)){3,5}(?=[^.]|$)/u' => '… ',  // fancy ellipses
+            '/\s*-{2,}\s*/u' => '—',                                // fancy emdash
+            '/(?:-(?=[”’,.;:!?\x{2026}]))|(?:(?<=[“‘])-)/u' => '—', // fancy emdash
+            '/\s+([.,;:!?\x{2026}])/u' => '$1',                     // correct preceeding whitespace
+            '/(?<=[\w])\(\s+/u' => ' (',                            // correct preceding whitespace
+            '/([,.;:!?)\x{2026}])\s*(?=[\w])/u' => '$1 ',           // correct trailing whitespace
+            '/(\w\.)\s*(?=\w\.)/u' => '$1',                         // correct acronym period spacing
+            '|(?<=[\w])\s*/\s*(?=[\w])|u' => ' / ',                 // correct surrounding whitespace
+            '/^\s*(.+?)\s*$/u' => "$1",                             // remove wrapping whitespace
         ];
         foreach ($map as $from => $to) {
             $paragraph = preg_replace($from, $to, $paragraph);
