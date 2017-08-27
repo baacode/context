e17d138d024bade30b0ed0af6773a11c3b479dcb
Erayd <steve@erayd.net>
2017-08-27 20:59:42 +1200

    Add main API

---
+++ src/Context.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Context;
+
+/**
+ * Main user API
+ *
+ * @package erayd/context
+ * @copyright (c) 2017 Erayd LTD
+ * @author Steve Gilberd <steve@erayd.net>
+ * @license ISC
+ */
+class Context
+{
+    /** @var Filter Filter instance */
+    protected $filter = null;
+
+    /**
+     * Create a new content extractor
+     *
+     * @param string $content
+     * @param string $filterClass
+     */
+    public function __construct(string $content, string $filterClass = HtmlFilter::class)
+    {
+        $this->filter = new $filterClass($content);
+    }
+
+    /**
+     * Render content
+     *
+     * @param int $flags Render flags
+     * @param string $renderClass
+     * @return string
+     */
+    public function render(int $flags = Render::RENDER_NONE, string $renderClass = MarkdownRender::class) : string
+    {
+        $r = new $renderClass($this->filter);
+        return $r->render($flags);
+    }
+}
